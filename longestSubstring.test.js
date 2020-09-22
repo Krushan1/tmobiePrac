cc1ec501faa15267e359fc7f7f6ceb72dbb73ad5
krushanpatel <krushan1795@gmail.com>
2020-09-22 10:38:36 -0400

    push

--- longestSubstring.test.js
+++
@@ -1,8 +0,0 @@
-const { exportAllDeclaration } = require('@babel/types');
-const lengthOfLongestSubstring = require('./longestSubstring');
-
-
-test('Testing substring', () => {
-  
-    expect(lengthOfLongestSubstring("abcabcbb")).toBe(3);
-})
